df3679068a058c2268c8279ce16e15ffee7e1b4e
MM <meyerm1@gmail.com>
2020-06-22 20:04:22 +0200

    don't use DeployedObjects anymore

--- Core.cs
+++ Core.cs
@@ -9,118 +9,109 @@ using SharpDX;
 
 namespace MineDetonator
 {
-    public class Core : BaseSettingsPlugin<Settings>
-    {
-        private DateTime LastDetonTime;
+	public class Core : BaseSettingsPlugin<Settings>
+	{
+		private DateTime LastDetonTime;
 
-        public override void Render()
-        {
-            if (!GameController.InGame)
-                return;
-            var actor = GameController.Player.GetComponent<Actor>();
-            var deployedObjects = actor.DeployedObjects.Where(x => x.Entity != null && x.Entity.Path.Contains("Metadata/MiscellaneousObjects/RemoteMine")).ToList();
+		public override void Render()
+		{
+			if (!GameController.InGame)
+				return;
+			if (!GameController.Player.Buffs.Any(x => x.Name.Equals("mine_mana_reservation", StringComparison.Ordinal)))
+				return;
+			var actor = GameController.Player.GetComponent<Actor>();
+			var realRange = Settings.DetonateDist.Value;
+			var mineSkill = actor.ActorSkills.Find(x => x.Name.IndexOf("mine", StringComparison.OrdinalIgnoreCase) >= 0);
+			if (mineSkill != null)
+			{
+				if (mineSkill.Stats.TryGetValue(GameStat.TotalSkillAreaOfEffectPctIncludingFinal, out var areaPct))
+				{
+					realRange += realRange * areaPct / 100f;
+					Settings.CurrentAreaPct.Value = realRange;
+				}
+				else
+				{
+					Settings.CurrentAreaPct.Value = 100;
+				}
+			}
+			else
+			{
+				Settings.CurrentAreaPct.Value = 0;
+			}
 
-            var realRange = Settings.DetonateDist.Value;
-            var mineSkill = actor.ActorSkills.Find(x => x.Name.ToLower().Contains("mine"));
-            if (mineSkill != null)
-            {
-                if (mineSkill.Stats.TryGetValue(GameStat.TotalSkillAreaOfEffectPctIncludingFinal, out var areaPct))
-                {
-                    realRange += realRange * areaPct / 100f;
-                    Settings.CurrentAreaPct.Value = realRange;
-                }
-                else
-                {
-                    Settings.CurrentAreaPct.Value = 100;
-                }
-            }
-            else
-            {
-                Settings.CurrentAreaPct.Value = 0;
-            }
+			var playerPos = GameController.Player.GridPos;
 
+			_monsters = _monsters.Where(x => x.IsAlive).ToList();
 
-            if (deployedObjects.Count == 0)
-            {
-                return;
-            }
-
-            var playerPos = GameController.Player.GridPos;
-
-            _monsters = _monsters.Where(x => x.IsAlive).ToList();
-
-            var nearMonsters = _monsters.Where(x => x != null &&
-	            !x.GetComponent<Life>().HasBuff("hidden_monster") && 
-	            !x.GetComponent<Life>().HasBuff("avarius_statue_buff") && 
+			var nearMonsters = _monsters.Where(x => x != null &&
+				!x.GetComponent<Life>().HasBuff("hidden_monster") && 
+				!x.GetComponent<Life>().HasBuff("avarius_statue_buff") && 
 				!x.GetComponent<Life>().HasBuff("hidden_monster_disable_minions") &&
-                FilterNullAction(x.GetComponent<Actor>()) &&
+				FilterNullAction(x.GetComponent<Actor>()) &&
 				x.GetComponent<Actor>().CurrentAction?.Skill?.Name != "AtziriSummonDemons" &&
 				x.GetComponent<Actor>().CurrentAction?.Skill?.Id != 728 &&//Lab?
-	            Vector2.Distance(playerPos, x.GridPos) < realRange).ToList();
+				Vector2.Distance(playerPos, x.GridPos) < realRange).Take(1).ToList();
 
-            if (nearMonsters.Count == 0)
-                return;
+			if (nearMonsters.Count == 0)
+				return;
 
-	        Settings.TriggerReason = "Path: " + nearMonsters[0].Path;
+			Settings.TriggerReason = "Path: " + nearMonsters[0].Path;
 
-            if(Settings.Debug.Value)
-			    LogMessage($"Ents: {nearMonsters.Count}. Last: {nearMonsters[0].Path}", 2);
+			if(Settings.Debug.Value)
+				LogMessage($"Ents: {nearMonsters.Count}. Last: {nearMonsters[0].Path}", 2);
 
-            if ((DateTime.Now - LastDetonTime).TotalMilliseconds > Settings.DetonateDelay.Value)
-            {
-                if (deployedObjects.Any(x => x.Entity != null && x.Entity.IsValid && x.Entity.GetComponent<Stats>().StatDictionary[GameStat.CannotDie] == 0))
-                {
-                    LastDetonTime = DateTime.Now;
-                    Keyboard.KeyPress(Settings.DetonateKey.Value);
-                }
-            }
-        }
+			if ((DateTime.UtcNow - LastDetonTime).TotalMilliseconds > Settings.DetonateDelay.Value)
+			{
+				LastDetonTime = DateTime.UtcNow;
+				Keyboard.KeyPress(Settings.DetonateKey.Value);
+			}
+		}
 
-        private List<Entity> _monsters = new List<Entity>();
+		private List<Entity> _monsters = new List<Entity>();
 
-        #region Overrides of BasePlugin
+		#region Overrides of BasePlugin
 
-        public override void EntityAdded(Entity entityWrapper)
-        {
-            if (entityWrapper == null)
-                return;
+		public override void EntityAdded(Entity entityWrapper)
+		{
+			if (entityWrapper == null)
+				return;
 
-            if (!entityWrapper.HasComponent<Monster>())
-                return;
+			if (!entityWrapper.HasComponent<Monster>())
+				return;
 
-            if (!entityWrapper.IsAlive)
-                return;
+			if (!entityWrapper.IsAlive)
+				return;
 
-            if (!entityWrapper.IsHostile)
-                return;
+			if (!entityWrapper.IsHostile)
+				return;
 
-	        if (entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalTaserNet") ||
-	            entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalUpgrades/UnholyRelic") ||
-	            entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalUpgrades/BetrayalDaemonSummonUnholyRelic"))
-		        return;
+			if (entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalTaserNet") ||
+				entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalUpgrades/UnholyRelic") ||
+				entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalUpgrades/BetrayalDaemonSummonUnholyRelic"))
+				return;
 
-            _monsters.Add(entityWrapper);
-        }
+			_monsters.Add(entityWrapper);
+		}
 
-        private bool FilterNullAction(Actor actor)
-        {
-            if (Settings.FilterNullAction.Value)
-                return actor.CurrentAction != null;
+		private bool FilterNullAction(Actor actor)
+		{
+			if (Settings.FilterNullAction.Value)
+				return actor.CurrentAction != null;
 
-            return true;
-        }
+			return true;
+		}
 
-        public override void EntityRemoved(Entity entityWrapper)
-        {
-            if (entityWrapper == null)
-                return;
+		public override void EntityRemoved(Entity entityWrapper)
+		{
+			if (entityWrapper == null)
+				return;
 
-            if (!entityWrapper.HasComponent<Monster>())
-                return;
+			if (!entityWrapper.HasComponent<Monster>())
+				return;
 
-            _monsters.Remove(entityWrapper);
-        }
+			_monsters.Remove(entityWrapper);
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
